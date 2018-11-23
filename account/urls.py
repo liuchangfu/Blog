acd780c45f36a8dcfc22c343d7bc568c14c95f9e
liuchangfu <shift_1220@163.com>
2018-11-23 18:35:20 +0800

    增加内置登录方法

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,12 @@
 from django.urls import path
 from . import views
 from django.conf import settings
+from django.contrib.auth import views as auth_views
+
 app_name = 'account'
 
 urlpatterns = [
-    path('login/', views.user_login, name='user_login'),
+    # path('login/', views.user_login, name='user_login'), # 自定义登录
+    path('login/', auth_views.auth_login, name='user_login'), # django内置登录
+    path('new-login/', auth_views.auth_login, {'template_name': 'account/login.html'}, name='user_login'),
 ]
