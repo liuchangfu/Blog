2e1e6de3d389bf50da75ee0a6b09d1d784302ee0
liuchangfu <shift_1220@163.com>
2018-12-04 14:02:58 +0800

    修改后台管理中模玛型中的字段名称用中文显示

--- article/__init__.py
+++ article/__init__.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 import os
 
-default_app_config = 'article.PrimaryBlogConfig'
+default_app_config = 'article.PrimaryArticlesConfig'
 
 VERBOSE_APP_NAME = "文章管理"
 
@@ -10,6 +10,6 @@ def get_current_app_name(_file):
     return os.path.split(os.path.dirname(_file))[-1]
 
 
-class PrimaryBlogConfig(AppConfig):
+class PrimaryArticlesConfig(AppConfig):
     name = get_current_app_name(__file__)
     verbose_name = VERBOSE_APP_NAME
