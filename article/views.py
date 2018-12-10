b16e0930ad3959f3079a7dacb0ea9a0374d26e94
liuchangfu <shift_1220@163.com>
2018-12-10 08:43:25 +0800

    增加分页功能

--- article/views.py
+++ article/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
@@ -83,8 +84,19 @@ def article_post(request):
 
 @login_required(login_url='/account/login')
 def article_list(request):
-    articles = ArticlePost.objects.filter(author=request.user)
-    return render(request, 'article/column/article_list.html', {'articles': articles})
+    articles_list = ArticlePost.objects.filter(author=request.user)
+    paginator = Paginator(articles_list, 2)
+    page = request.GET.get('page')
+    try:
+        current_page = paginator.page(page)
+        articles = current_page.object_list
+    except PageNotAnInteger:
+        current_page = paginator.page(1)
+        articles = current_page.object_list
+    except EmptyPage:
+        current_page = paginator.page(paginator.num_pages)
+        articles = current_page.object_list
+    return render(request, 'article/column/article_list.html', {'articles': articles, 'page': current_page})
 
 
 @login_required(login_url='/account/login')
