5cff8231d5612586a47bbdf87936410cecf7a3f1
liuchangfu <shift_1220@163.com>
2018-12-18 09:06:05 +0800

    增加统计文章总数功能

---
+++ article/templatetags/article_tags.py
@@ -0,0 +1,15 @@
+# _*_ coding:utf-8 _*_
+from django import template
+register = template.Library()
+
+from article.models import ArticlePost
+
+
+@register.simple_tag
+def total_articles():
+    return ArticlePost.objects.count()
+
+
+@register.simple_tag
+def author_total_articles(user):
+    return user.article.count()
