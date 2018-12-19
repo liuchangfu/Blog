5756845fe7ba85dc9829b84535b6e28128326448
liuchangfu <shift_1220@163.com>
2018-12-19 09:11:25 +0800

    增加评论最多功能

--- article/templatetags/article_tags.py
+++ article/templatetags/article_tags.py
@@ -2,7 +2,10 @@
 from django import template
 register = template.Library()
 
+from django.db.models import Count
 from article.models import ArticlePost
+from django.utils.safestring import mark_safe
+import markdown
 
 
 @register.simple_tag
@@ -13,3 +16,19 @@ def total_articles():
 @register.simple_tag
 def author_total_articles(user):
     return user.article.count()
+
+
+@register.inclusion_tag('article/list/latest_article.html')
+def latest_articles(n=5):
+    latest_articles = ArticlePost.objects.order_by('-created')[:n]
+    return {'latest_articles': latest_articles}
+
+
+@register.simple_tag()
+def most_commented_articles(n=3):
+    return ArticlePost.objects.annotate(total_comments=Count('comments')).order_by("-total_comments")[:n]
+
+
+@register.filter(name='markdown')
+def markdown_filter(text):
+    return mark_safe(markdown.markdown(text))
